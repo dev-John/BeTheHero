95313197417643285d9a50ad1a9cb64a064387ab
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2020-03-26 22:28:40 -0300

    Frontend, paginas de cadastro, login e início da pagina de Profile.

---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon/index';
+import Register from './pages/Register/index';
+import Profile from './pages/Profile/index';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
